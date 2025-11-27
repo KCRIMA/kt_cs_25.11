13d995e450ff31e93fe658fc97dcb0292c887140
KCRIMA <tbvj61@gmail.com>
2025-11-27 23:14:32 +0900

    11.27_선택사항 수정

--- src/main/java/com/kt_cs/kt_cs_2511/config/DashApiProperties.java
+++ src/main/java/com/kt_cs/kt_cs_2511/config/DashApiProperties.java
@@ -1,8 +1,12 @@
 package com.kt_cs.kt_cs_2511.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Getter
+@Setter
 @Component
 @ConfigurationProperties(prefix = "dash.api")
 public class DashApiProperties {
